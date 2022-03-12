ac71d809681c16733e8d7404a333aedff64b7b66
AlvinLin <alvinvalensia24@gmail.com>
2022-03-12 03:26:36 -0600

    membuat component baru MovieCard.jsx dan melakukan mapping

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
 
 import "./App.css";
 import SearchIcon from "./search.svg";
 
 const API_URL = "https://www.omdbapi.com?apikey=57396eb";
 
+const movie1 = {
+  Title: "Amazing Spiderman Syndrome",
+  Year: "2012",
+  imdbID: "tt2586634",
+  Type: "movie",
+  Poster: "N/A",
+};
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
 
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
   useEffect(() => {
     searchMovies("Spiderman");
@@ -27,7 +39,20 @@ const App = () => {
           value="Superman"
           onChange={() => {}}
         />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
